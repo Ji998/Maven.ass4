b563c10e3474371b0ec6d63a423a14d173b5b2d8
Adrian Roszkowski <adr11@hi.is>
2023-02-14 10:32:58 +0000

    fixed cmd files

--- runjar.cmd
+++ runjar.cmd
@@ -1,1 +1,1 @@
-java -cp target\Maven-1.0-SNAPSHOT.jar;target\dependency.commons-numbers-primes-1.1.jar is.hi.xij1.hbv202g.ass4.App
+java -cp target\Maven-1.0-SNAPSHOT.jar;target\dependency\commons-numbers-primes-1.1.jar is.hi.xij1.hbv202g.ass4.App
